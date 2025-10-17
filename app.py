cfdf64ed0f2975998b3f65c66339e5720aa2b24a
Zazimi3131383 <zahra.azimi3131383@gmail.com>
2025-10-17 13:30:17 +0330

    Update app.py

--- app.py
+++ app.py
@@ -339,4 +339,7 @@ def download_csv():
     return send_file(CSV_FILE, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
